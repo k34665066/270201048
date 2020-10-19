6a1c696e08dc86b0f6bb343b95f87ff2dc3422c5
k34665066 <melihcakmak1755@gmail.com>
2020-10-19 09:52:56 +0000

    lab2

---
+++ lab2/example5.py
@@ -0,0 +1,4 @@
+side1=float(input("side1:"))
+side2=float(input("side2:"))
+hypotenuse=(side1**2+side2**2)**(1/2)
+print(hypotenuse)
---
+++ lab2/example6.py
@@ -0,0 +1,6 @@
+car1=80
+car2=70
+distance1=490
+i=490-150
+minute=60*(i/(car1+car2))
+print(minute)
