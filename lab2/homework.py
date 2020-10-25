527b9b1f7e9479a4de02f8d3f5919807b8c57313
k34665066 <melihcakmak1755@gmail.com>
2020-10-25 20:26:21 +0000

    hw

--- lab2/homework.py
+++ lab2/homework.py
@@ -1,3 +1,4 @@
+#If I leave my house at 6:52 am and run 1 mile at an easy pace (8 minutes per mile), then 3 miles at tempo (6 minutes per mile) and 2 mile at easy pace again, what time do I get home for breakfast?
 time=6.52
 minutes_time=6*60+52
 easy_peace=8
