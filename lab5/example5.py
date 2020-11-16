bc44c7982c454eeb4d7b17d3e5ef79426046227b
k34665066 <melihcakmak1755@gmail.com>
2020-11-16 10:04:18 +0000

    lab5

---
+++ lab5/example5.py
@@ -0,0 +1,10 @@
+
+fibonacci_list=list()
+fibonacci_list.append(1)
+fibonacci_list.append(1)
+index=2
+how_many=int(input("How many fibonacci numbers do you see"))
+for a in range(how_many-2):
+    fibonacci_list.append(fibonacci_list[index-1]+fibonacci_list[index-2])
+    index+=1
+print(fibonacci_list)
