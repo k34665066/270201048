29da77531e4a27cbeb2fb67a8b7d7838998d32a6
k34665066 <melihcakmak1755@gmail.com>
2020-12-21 09:45:00 +0000

    lab9

---
+++ lab9/example3.py
@@ -0,0 +1,11 @@
+def get_evens(my_list):
+    if len(my_list)==0:
+        return 0
+    #recursive part
+    counter=0
+    if my_list[0]%2==0:
+        counter=1
+    return counter + get_evens(my_list[1:])
+print(get_evens([0,1,2,3,4,5]))
+        
+
---
+++ lab9/example4.py
@@ -0,0 +1,9 @@
+import time
+def timer(t):
+    if t==0:
+        print("Finished")
+        return 0
+    print("Remaining time",t)
+    time.sleep(1)
+    return timer(t-1)
+timer(5)
