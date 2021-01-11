675329b1c799881091a0a1136c5b058737e3ff5e
k34665066 <melihcakmak1755@gmail.com>
2021-01-11 08:52:45 +0000

    lab11

---
+++ lab11/example1.py
@@ -0,0 +1,14 @@
+class cylinder:
+
+    def __init__(self,radius,height):
+        self.radius=radius
+        self.height=height
+    def base_area(self):
+        return 3.141592653589793*(self.radius**2)
+    def surface_area(self):
+        return 2*3.141592653589793*self.radius*self.height
+    def volume(self):
+        return 2*self.base_area()*self.surface_area()
+cylinder1=cylinder(3,5)
+print(cylinder1.volume())
+
